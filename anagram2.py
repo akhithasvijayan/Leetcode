6e2a117a37ccf82006394e93855a9e11bf74af4b
Akhitha S Vijayan <formystudy2007@gmail.com>
2026-02-17 11:36:46 +0530

    Add files via upload

---
+++ anagram2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        groups = {}
+
+        for num in range(1, n + 1):
+            digit_sum = sum(int(d) for d in str(num))
+            groups[digit_sum] = groups.get(digit_sum, 0) + 1
+
+        max_size = max(groups.values())
+
+        return sum(1 for size in groups.values() if size == max_size)
