fefe2574c9373b666ff317b5eeae7ddb9d06d783
Akhitha S Vijayan <formystudy2007@gmail.com>
2026-05-26 19:16:09 +0530

    Add files via upload

---
+++ contains.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+
+        mp = {}
+
+        for i in range(len(nums)):
+
+            if nums[i] in mp:
+
+                if i - mp[nums[i]] <= k:
+                    return True
+
+            mp[nums[i]] = i
+
+        return False
