3d8c9abe2820d4a159b294106d252a537f795e33
Akhitha S Vijayan <formystudy2007@gmail.com>
2026-06-12 08:43:03 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x):
+        self.queue.append(x)
+
+        # Move old elements behind the new one
+        for i in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self):
+        return self.queue.popleft()
+
+    def top(self):
+        return self.queue[0]
+
+    def empty(self):
+        return len(self.queue) == 0
