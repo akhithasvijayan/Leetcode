524e2a0adef0e4b20a252abd56eac265d74e179a
Akhitha S Vijayan <formystudy2007@gmail.com>
2026-09-14 21:49:05 +0530

    Add files via upload

---
+++ ship.py
@@ -0,0 +1,25 @@
+class Solution:
+    def shipWithinDays(self, weights, days):
+
+        left = max(weights)
+        right = sum(weights)
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            current_weight = 0
+            required_days = 1
+
+            for weight in weights:
+                if current_weight + weight > mid:
+                    required_days += 1
+                    current_weight = 0
+
+                current_weight += weight
+
+            if required_days <= days:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
