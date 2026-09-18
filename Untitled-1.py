b1e52522b536ba69c7473b4928784e289696f692
Akhitha S Vijayan <formystudy2007@gmail.com>
2026-09-18 13:45:18 +0530

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,24 +1,13 @@
 class Solution:
-    def splitArray(self, nums, k):
-        left = max(nums)
-        right = sum(nums)
+    def hasCycle(self, head):
+        slow = head
+        fast = head
 
-        while left <= right:
-            mid = (left + right) // 2
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
-            count = 1
-            current_sum = 0
+            if slow == fast:
+                return True
 
-            for num in nums:
-                if current_sum + num > mid:
-                    count += 1
-                    current_sum = num
-                else:
-                    current_sum += num
-
-            if count <= k:
-                right = mid - 1
-            else:
-                left = mid + 1
-
-        return left
+        return False
